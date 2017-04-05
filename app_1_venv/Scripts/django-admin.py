461d1f6eb89d5d8d6c273f893b382abc5489edc3
mark <markusemailbox@gmail.com>
2017-04-05 12:46:18 +0800

    app01, first commit

---
+++ app_1_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mark\documents\web development\projects\learn_django\app_1\app_1_venv\scripts\python3.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
